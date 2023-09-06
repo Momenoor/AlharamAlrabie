61eb5882f421ba877388d83d840ade243058aa1f
Momen Noor <momen.noor@gmail.com>
2023-09-06 09:52:54 +0400

    Update

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/menu',[App\Http\Controllers\HomeController::class, 'menu'])->name('menu');
+
 Auth::routes();
 
 Route::middleware('auth')->group(function () {
