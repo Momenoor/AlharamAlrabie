a69ecbe7fde49a01056532e60ce2611e2b7ec560
Momen Noor <momen.noor@gmail.com>
2023-09-25 18:40:39 +0400

    Update

--- resources/views/themeOne/layouts/home/app.blade.php
+++ resources/views/themeOne/layouts/home/app.blade.php
@@ -5,6 +5,7 @@
     <base href=""/>
     <title>{{ config('app.name', 'Laravel') }}</title>
     <meta charset="utf-8"/>
+    <meta name="facebook-domain-verification" content="xec5bz7gz20ong9u4byzmcdcrx5n95" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
